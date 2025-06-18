aa0a491f98bed1158e06a730500c21c4cdaf641b
NAVEEN-RAJ-V <naveenraj246800@gmail.com>
2025-06-18 10:48:49 +0530

    Update frontend and backend with latest changes

--- Newfolder/server.js
+++ Newfolder/server.js
@@ -16,14 +16,35 @@ const statisticsRoutes = require("./routes/statistics")
 const app = express()
 
 // Middleware
+// app.use(
+//   cors({
+//     origin: ["http://localhost:3000", "https://slot-management-iota.vercel.app", process.env.FRONTEND_URL],
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   }),
+// )
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://slot-management-iota.vercel.app",
+  "https://slot-management-git-main-naveen-raj-vs-projects.vercel.app",
+]
+
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://slot-management-iota.vercel.app", process.env.FRONTEND_URL],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error("Not allowed by CORS"))
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-  }),
+  })
 )
+
 
 app.options("*", cors())
 app.use(express.json())
