758e2450870f0c273f0b755a28dd87f4752239d6
NAVEEN-RAJ-V <naveenraj246800@gmail.com>
2025-06-18 14:34:57 +0530

    Update frontend and backend with latest changes

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ app.use(
       "http://localhost:3000",
       "https://slot-management-iota.vercel.app",// Replace with your actual frontend domain
       "https://slot-management-git-main-naveen-raj-vs-projects.vercel.app",
-      "https://slot-management-1chftnk0u-naveen-raj-vs-projects.vercel.app",
       process.env.FRONTEND_URL,
     ],
     credentials: true,
